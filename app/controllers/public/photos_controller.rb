43b6d72b02c97f1157db58130f00ccec919c2387
ayakatonton <145669983+ayakatonton@users.noreply.github.com>
2023-12-14 21:21:08 +0000

    arubamu

--- app/controllers/public/photos_controller.rb
+++ app/controllers/public/photos_controller.rb
@@ -3,16 +3,31 @@ class Public::PhotosController < ApplicationController
   end
 
   def show
+    @photo = Photo.find(params[:id])
   end
 
   def new
+    @photo = Photo.new
   end
 
   def edit
+  end
+  
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to @photo, notice: '写真を共有しました'
+    else
+      render :new
+    end
   end
   
   def photo_params
     params.require(:photo).permit(:title, :body, :image)
   end
   
+  private
+  def photo_params
+    params.require(:photo).permit(:title, :description, :image)
+  end
 end
