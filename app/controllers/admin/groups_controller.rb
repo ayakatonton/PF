43522e469e8a41b8f2b3ed1f2e73c80ac56d9dd3
ayakatonton <145669983+ayakatonton@users.noreply.github.com>
2023-12-25 13:28:49 +0000

    レイアウト

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -1,7 +1,7 @@
 class Admin::GroupsController < ApplicationController
   before_action :authenticate_admin!
   def index
-    @groups = Group.all
+    @groups = Group.all.page(params[:page]).per(5)
     @groups = @groups.where("name LIKE ?", "%#{params[:search]}%") if params[:search].present?
     # 検索機能：名前で検索する場合。他の検索条件がある場合は適宜追加してください。
   end 
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
   
   def index
-    @users = User.all
+    @users = User.all.page(params[:page]).per(5)
     @users = @users.where("name LIKE ?", "%#{params[:search]}%") if params[:search].present?
     # 検索機能
   end
